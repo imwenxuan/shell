07ab29b8d5c9d75fc020545276cd06d68b5efe88
imwenxuan <46060039+imwenxuan@users.noreply.github.com>
2020-04-21 18:49:41 +0800

    Update auto_create_user_plus.sh

--- auto_create_user_plus.sh
+++ auto_create_user_plus.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/bash
 #Create user Version 1.2
 #Author by Wenxuan
-#From The Chinese Chungking City,PRC,Asia
+while true
+do
+#請輸入需要創建賬戶的前綴
+read  -p   "please input the prefix: "   prefix
+	if [ ! -z "$prefix" ]
+		then
+		break
+	else
+		echo "error,please retry"
+	fi
+done
 while true
 do
 #請輸入需要創建賬戶的數目
@@ -14,38 +24,25 @@ do
 		 
 	fi
 done
-
-while true
-do
-#請輸入需要創建賬戶的前綴
-read  -p   "please input the prefix: "   prefix
-	if [ ! -z "$prefix" ]
-		then
-		break
-	else
-		echo "error,please retry"
-		
-	
-	
-	fi
-
-done
-
 #開始創建賬戶
-
 for  i  in `seq $number`
 do
 	user=$prefix$i
-
-	useradd  $user
-
-	echo "123" |passwd --stdin  $user &>/dev/null
-
-	if [ $? -eq 0 ]
+	id $user &>/dev/null
+	if [  $? -eq 0  ]
 	then
-		#下次登陸强制修改密碼
-		chage -d 0  $user
-		echo "$user is created successfully"
-		echo ""默認密碼:123!
+			echo "$user 已经存在"
+	else
+		useradd  $user &>/dev/null
+		echo "123" |passwd --stdin  $user &>/dev/null
+
+		if [ $? -eq 0 ]
+		then
+			#下次登陸强制要求修改密碼
+			chage -d 0  $user
+			echo "$user is created successfully"
+			echo "默認密碼:123!"
 		fi
+	fi
 done
+
